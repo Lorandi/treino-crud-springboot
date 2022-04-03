c58c3585546a5c86b6822cbe0b960c69a1aea7f0
Rodrigo Lorandi <rodrigo_lorandi@hotmail.com>
2022-04-03 08:54:56 -0300

    config H2 database, test profile, JPA

--- src/main/java/com/rodrigolorandi/crudStudy/entities/User.java
+++ src/main/java/com/rodrigolorandi/crudStudy/entities/User.java
@@ -4,19 +4,21 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
-
-import java.io.Serializable;
-
-//@Entity
+@Entity
 //@Table(name = "user")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class User{
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
     private String phone;
     private String password;
