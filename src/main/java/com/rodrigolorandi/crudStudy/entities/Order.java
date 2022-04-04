9fe32a09b3f574f9dc3ae01c202d1c1d2aea0f6a
Rodrigo Lorandi <rodrigo_lorandi@hotmail.com>
2022-04-03 21:38:18 -0300

    Order, Instant, ISO 8601 - parte 1

---
+++ src/main/java/com/rodrigolorandi/crudStudy/entities/Order.java
@@ -0,0 +1,26 @@
+package com.rodrigolorandi.crudStudy.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.Instant;
+
+@Entity
+@Table(name = "tb_order")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Instant moment;
+
+    @ManyToOne
+    @JoinColumn(name = "client_id")
+    private User client;
+}
--- src/main/java/com/rodrigolorandi/crudStudy/entities/User.java
+++ src/main/java/com/rodrigolorandi/crudStudy/entities/User.java
@@ -4,13 +4,12 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
-//@Table(name = "user")
+@Table(name = "tb_user")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -23,5 +22,16 @@ public class User{
     private String email;
     private String phone;
     private String password;
+
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
+
+    public User(Long id, String name, String email, String phone, String password) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.password = password;
+    }
 
 }
