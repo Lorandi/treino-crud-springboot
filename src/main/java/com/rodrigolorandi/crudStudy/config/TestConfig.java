5e4bda683ad1d7a8ed9e405d4801c0e648680dff
Rodrigo Lorandi <rodrigo_lorandi@hotmail.com>
2022-04-08 10:35:59 -0300

    Product-OrderItem one-to-many association

--- src/main/java/com/rodrigolorandi/crudStudy/config/TestConfig.java
+++ src/main/java/com/rodrigolorandi/crudStudy/config/TestConfig.java
@@ -62,7 +62,6 @@ public class TestConfig implements CommandLineRunner {
         OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
         OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
         OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
-//        OrderItem oi5 = new OrderItem(o3, null, null, null);
 
         orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
     }
